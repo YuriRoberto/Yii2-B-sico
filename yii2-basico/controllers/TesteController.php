df551246b631eadfb1c8300342f50990e5154dc4
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-02-01 12:01:13 -0300

    Parametros de rotas e views

---
+++ yii2-basico/controllers/TesteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+class TesteController extends \yii\web\Controller
+{
+    public function actionIndex($id = 500)
+    {
+        $request = \Yii::$app->request;
+
+        $xpto = $request->get('xpto');
+        return $this->render('index', [
+            'id' => $id,
+            'xpto' => $xpto
+        ]);
+    }
+
+}
