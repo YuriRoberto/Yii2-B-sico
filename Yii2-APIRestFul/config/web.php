85835673720efac774381e3bd4cc13b8ba4fa2f3
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-02-05 12:40:31 -0300

    Criando primeiro controller Restful

--- Yii2-APIRestFul/config/web.php
+++ Yii2-APIRestFul/config/web.php
@@ -48,6 +48,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'employees']
             ],
         ],
         
