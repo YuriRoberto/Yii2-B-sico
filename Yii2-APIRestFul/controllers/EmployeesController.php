31ea691f0cb9a7b6ba10826c7b94657c2d3a8857
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-02-06 09:38:59 -0300

    CRUD/Content Negotiator

--- Yii2-APIRestFul/controllers/EmployeesController.php
+++ Yii2-APIRestFul/controllers/EmployeesController.php
@@ -3,10 +3,23 @@
 namespace app\controllers;
 
 use yii\rest\ActiveController;
+use yii\web\Response;
 
 class EmployeesController extends ActiveController
 {
 
     public $modelClass = "app\models\Employee";
+
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvolope' => 'data'
+    ];
+
+    public function behaviors(){
+
+        $b = parent::behaviors();
+        $b['contentNegotiator']['formats']['text/html'] = Response::FORMAT_HTML;
+        return $b;
+    }
 
 }
